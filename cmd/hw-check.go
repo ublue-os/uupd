f72a120af31b15df45e9b54264199a7101ea67e1
gerblesh <101901964+gerblesh@users.noreply.github.com>
2026-01-09 00:09:29 -0800

    add config file and management with viper

--- cmd/hw-check.go
+++ cmd/hw-check.go
@@ -1,16 +1,20 @@
 package cmd
 
 import (
+	"log/slog"
+
 	"github.com/spf13/cobra"
 	"github.com/ublue-os/uupd/checks"
-	"log"
 )
 
-func HwCheck(cmd *cobra.Command, args []string) {
+func HwCheck(cmd *cobra.Command, args []string) error {
 	// (some hardware checks require dbus access)
 	err := checks.RunHwChecks()
 	if err != nil {
-		log.Fatalf("Hardware checks failed: %v", err)
+		slog.Error("Hardware checks failed: %v", err)
+		return err
 	}
-	log.Println("Hardware checks passed")
+	slog.Info("Hardware checks passed")
+
+	return nil
 }
--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/ublue-os/uupd/pkg/config"
 	appLogging "github.com/ublue-os/uupd/pkg/logging"
 	"golang.org/x/term"
 )
@@ -27,57 +29,71 @@ func assertRoot(cmd *cobra.Command, args []string) {
 
 var (
 	rootCmd = &cobra.Command{
-		Use:               "uupd",
-		Short:             "uupd (Universal Update) is the successor to ublue-update, built for bootc",
-		PersistentPreRunE: initLogging,
-		PreRun:            assertRoot,
-		Run:               Update,
+		Use:    "uupd",
+		Short:  "uupd (Universal Update) is the successor to ublue-update, built for bootc",
+		PreRun: assertRoot,
+		RunE:   Update,
 	}
 
 	waitCmd = &cobra.Command{
 		Use:    "wait",
 		Short:  "Waits for ostree sysroot to unlock",
 		PreRun: assertRoot,
-		Run:    Wait,
+		RunE:   Wait,
 	}
 
 	updateCheckCmd = &cobra.Command{
 		Use:    "update-check",
 		Short:  "Check for updates to the booted image, returns exit code 77 if update is not available",
 		PreRun: assertRoot,
-		Run:    UpdateCheck,
+		RunE:   UpdateCheck,
 	}
 
 	hardwareCheckCmd = &cobra.Command{
 		Use:    "hw-check",
 		Short:  "Run hardware checks",
 		PreRun: assertRoot,
-		Run:    HwCheck,
+		RunE:   HwCheck,
 	}
 
 	imageOutdatedCmd = &cobra.Command{
 		Use:    "is-img-outdated",
 		Short:  "Checks if the current booted image is over 1 month old, returns exit code 77 if true.",
 		PreRun: assertRoot,
-		Run:    ImageOutdated,
+		RunE:   ImageOutdated,
 	}
 
-	fLogFile   string
-	fLogLevel  string
-	fNoLogging bool
-	fLogJson   bool
+	fLogFile    string
+	fLogLevel   string
+	fNoLogging  bool
+	fLogJson    bool
+	fConfigPath string
 )
 
 func Execute() {
+	if err := initPre(); err != nil {
+		slog.Error("Unable to init", slog.Any("error", err))
+		os.Exit(1)
+	}
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		slog.Error("Command failed!", slog.Any("error", err))
 		os.Exit(1)
 	}
 }
 
-func initLogging(cmd *cobra.Command, args []string) error {
+func initPre() error {
+	if err := config.InitConfig(fConfigPath); err != nil {
+		return fmt.Errorf("Failed to init config: %v", err)
+	}
+	if err := initLogging(); err != nil {
+		return fmt.Errorf("Failed to init logging: %v", err)
+	}
+	return nil
+}
+
+func initLogging() error {
 	logWriter := os.Stdout
-	if fLogFile != "-" {
+	if fLogFile != "" {
 		abs, err := filepath.Abs(path.Clean(fLogFile))
 		if err != nil {
 			return err
@@ -93,7 +109,7 @@ func initLogging(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	main_app_logger := slog.New(appLogging.SetupAppLogger(logWriter, logLevel, fLogFile != "-" || fLogJson))
+	main_app_logger := slog.New(appLogging.SetupAppLogger(logWriter, logLevel, fLogFile != "" || fLogJson))
 
 	if fNoLogging {
 		slog.SetDefault(appLogging.NewMuteLogger())
@@ -104,24 +120,47 @@ func initLogging(cmd *cobra.Command, args []string) error {
 }
 
 func init() {
+
 	rootCmd.AddCommand(waitCmd)
 	rootCmd.AddCommand(updateCheckCmd)
 	rootCmd.AddCommand(hardwareCheckCmd)
 	rootCmd.AddCommand(imageOutdatedCmd)
+
+	// config flags
 	rootCmd.Flags().Bool("disable-module-system", false, "Disable the System module")
 	rootCmd.Flags().Bool("disable-module-flatpak", false, "Disable the Flatpak module")
 	rootCmd.Flags().Bool("disable-module-distrobox", false, "Disable the Distrobox update module")
 	rootCmd.Flags().Bool("disable-module-brew", false, "Disable the Brew update module")
-	rootCmd.Flags().BoolP("hw-check", "c", false, "Run hardware check before running updates")
+
+	rootCmd.PersistentFlags().BoolVar(&fLogJson, "json", false, "Print logs as json")
+	rootCmd.PersistentFlags().StringVar(&fLogFile, "log-file", "-", "File where user-facing logs will be written to")
+	rootCmd.PersistentFlags().StringVar(&fLogLevel, "log-level", "info", "Log level for user-facing logs")
+	// misc flags
+
 	rootCmd.Flags().BoolP("force", "f", false, "Force system update without update checks")
 	rootCmd.Flags().BoolP("dry-run", "n", false, "Do a dry run")
+	// rootCmd.Flags().Bool("auto", false, "Indicate that this is an automatic update")
+	rootCmd.PersistentFlags().StringVar(&fConfigPath, "config", config.DEFAULT_PATH, "Config file path")
+
 	rootCmd.Flags().BoolP("verbose", "v", false, "Display command outputs after run")
+
 	rootCmd.Flags().Bool("ci", false, "Makes some modifications to behavior if is running in CI")
 	isTerminal := term.IsTerminal(int(os.Stdout.Fd()))
 	rootCmd.Flags().Bool("disable-progress", !isTerminal, "Disable the GUI progress indicator, automatically disabled when loglevel is debug or in JSON")
 	rootCmd.Flags().Bool("apply", false, "Reboot if there's an update to the image")
-	rootCmd.PersistentFlags().BoolVar(&fLogJson, "json", false, "Print logs as json (used for testing)")
-	rootCmd.PersistentFlags().StringVar(&fLogFile, "log-file", "-", "File where user-facing logs will be written to")
-	rootCmd.PersistentFlags().StringVar(&fLogLevel, "log-level", "info", "Log level for user-facing logs")
 	rootCmd.PersistentFlags().BoolVar(&fNoLogging, "quiet", false, "Make logs quiet")
+
+	_ = viper.BindPFlag("modules.flatpak.disable", rootCmd.Flags().Lookup("disable-module-flatpak"))
+	_ = viper.BindPFlag("modules.brew.disable", rootCmd.Flags().Lookup("disable-module-brew"))
+	_ = viper.BindPFlag("modules.system.disable", rootCmd.Flags().Lookup("disable-module-system"))
+	_ = viper.BindPFlag("modules.distrobox.disable", rootCmd.Flags().Lookup("disable-module-distrobox"))
+	_ = viper.BindPFlag("checks.hardware.enable", rootCmd.Flags().Lookup("hw-check"))
+
+	// _ = viper.BindPFlag("update.force", rootCmd.Flags().Lookup("force"))
+	// _ = viper.BindPFlag("update.verbose", rootCmd.Flags().Lookup("verbose"))
+
+	_ = viper.BindPFlag("logging.json", rootCmd.PersistentFlags().Lookup("json"))
+	_ = viper.BindPFlag("logging.file", rootCmd.PersistentFlags().Lookup("log-file"))
+	_ = viper.BindPFlag("logging.level", rootCmd.PersistentFlags().Lookup("log-level"))
+	_ = viper.BindPFlag("logging.quiet", rootCmd.PersistentFlags().Lookup("quiet"))
 }
